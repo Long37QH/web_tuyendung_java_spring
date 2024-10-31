efa83e3a367ba50e08c3ac59127a7a45437d67e9
Dinh Viet Long <long37k1999@gmail.com>
2024-10-31 17:46:14 +0700

    hien thi list job len trang viec lam

--- src/main/java/vn/com/jobviet/controller/client/JobPageController.java
+++ src/main/java/vn/com/jobviet/controller/client/JobPageController.java
@@ -1,9 +1,15 @@
 package vn.com.jobviet.controller.client;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import vn.com.jobviet.domain.Job;
 import vn.com.jobviet.service.JobService;
@@ -19,7 +25,28 @@ public class JobPageController {
     }
 
     @GetMapping("/job")
-    public String getPageJob() {
+    public String getPageJob(Model model,@RequestParam("page") Optional<String> pageOptional) {
+        int page = 1;
+        try {
+            if (pageOptional.isPresent()) {
+                page = Integer.parseInt(pageOptional.get());
+            }else{
+                // page = 1
+            }
+        } catch (Exception e) {
+            // page = 1
+        }
+        PageRequest pageable = PageRequest.of(page - 1, 10);
+        String status = "Đăng bài";
+
+        Page<Job> prs = this.jobService.GetAllJobByStatusOderbyView(status,pageable);
+        List<Job> listjob = prs.getContent();
+
+        model.addAttribute("listjob", listjob);
+        //lây so trong hiện tại truyên sang view
+        model.addAttribute("curentPage", page);
+        // lấy tông số trang
+        model.addAttribute("totalPages", prs.getTotalPages());
         return "/client/job/show";
     }
 
